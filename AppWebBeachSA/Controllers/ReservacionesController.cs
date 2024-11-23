6c7c18b8df5cb2966439d1e8e5bf2ac2b80b7711
tintin1401 <dana.cardenascr@gmail.com>
2024-11-22 19:11:47 -0600

    home 25%

--- AppWebBeachSA/Controllers/ReservacionesController.cs
+++ AppWebBeachSA/Controllers/ReservacionesController.cs
@@ -33,7 +33,17 @@ namespace AppWebBeachSA.Controllers
                 reservaciones = JsonConvert.DeserializeObject<List<Reservacion>>(resultado);
             }
 
+
             if (reservaciones != null) {
+                 response = await httpClient.GetAsync("/Paquetes/Listado");
+                List<Paquete> paquetes = new List<Paquete>();
+                if (response.IsSuccessStatusCode)
+                {
+                    var resultado = response.Content.ReadAsStringAsync().Result;
+                    paquetes = JsonConvert.DeserializeObject<List<Paquete>>(resultado);
+                    ViewBag.Paquetes = paquetes;
+
+                }
                 return View(reservaciones);
             }
 
@@ -61,11 +71,36 @@ namespace AppWebBeachSA.Controllers
                 ViewBag.tipoCambio= data.venta;
             }
 
+
                 return View();
 
         }
 
+        [HttpGet]
 
+        public async Task<IActionResult> paquete(int id)
+        {
+            HttpResponseMessage response = await httpClient.GetAsync($"/Paquetes/Buscar?id={id}");
+            Paquete paquete = new Paquete();
+            if (response.IsSuccessStatusCode)
+            {
+                var resultado = response.Content.ReadAsStringAsync().Result;
+                paquete = JsonConvert.DeserializeObject<Paquete>(resultado);
+
+            }
+            var image = await httpClient.GetAsync("https://localhost:7160/Paquetes/Listado");
+            if (image.IsSuccessStatusCode)
+            {
+                var content =  image.Content.ReadAsStringAsync().Result;
+                var paquetes = JsonConvert.DeserializeObject<List<Paquete>>(content);
+                int indice = paquetes.FindIndex(paquete => paquete.PaqueteID == id);
+                ViewBag.image = indice+"Room.jpg";
+            }
+
+
+            return View(paquete);
+
+        }
 
 
     }
